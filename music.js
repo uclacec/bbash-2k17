e1e4f92d788e4c9982f0cd8eca9b6f4f9c4f7dc2
dhuangg <daniel.huang87@gmail.com>
2017-07-05 17:57:14 -0700

    changing background image

--- music.js
+++ music.js
@@ -4,5 +4,11 @@
 
 var madeon = new Audio('assets/music/Porter_Robinson__Madeon___Shelter_(Official_Audio).mp3');
 
-$('.madeon-button').on("mouseenter",function (){madeon.play(); });
-$('.madeon-button').on("mouseleave click",function(){madeon.pause();});
+$('.madeon-button').on("mouseenter",function (){
+    madeon.play();
+    $('#body').css({'background-image':'url(assets/images/porteon.png)'})
+});
+$('.madeon-button').on("mouseleave click",function(){
+    madeon.pause();
+    $('#body').css({'background-image':'url()'})
+});
