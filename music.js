8e42b527c5a2a12742d9e2190bef707e3fdcc273
dhuangg <daniel.huang87@gmail.com>
2017-07-05 17:31:00 -0700

    music on hover + cursor image

--- music.js
+++ music.js
@@ -1,3 +1,8 @@
 /**
  * Created by danielhuang on 7/5/17.
  */
+
+var madeon = new Audio('assets/music/Porter_Robinson__Madeon___Shelter_(Official_Audio).mp3');
+
+$('.madeon-button').on("mouseenter",function (){madeon.play(); });
+$('.madeon-button').on("mouseleave click",function(){madeon.pause();});
