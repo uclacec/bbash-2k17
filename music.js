798467dbbcfe368249be5bb9d3bfdc46cbe6ea1b
dhuangg <daniel.huang87@gmail.com>
2017-07-08 11:08:50 -0700

    responsive buttons setup

--- music.js
+++ music.js
@@ -4,11 +4,26 @@
 
 var madeon = new Audio('assets/music/Porter_Robinson__Madeon___Shelter_(Official_Audio).mp3');
 
-$('.madeon-button').on("mouseenter",function (){
+$('#music #top-row #b').on("mouseenter",function (){
     madeon.play();
-    $('#body').css({'background-image':'url(assets/images/porteon.png)'})
+    $('#body').css({'background-image':'url(assets/images/porteon.png)'});
 });
-$('.madeon-button').on("mouseleave click",function(){
+$('#music #top-row #b').on("mouseleave click",function(){
     madeon.pause();
     $('#body').css({'background-image':'url()'})
 });
+
+if($('window').width() <= 500){
+    //[b, r, u, i, n, ba, a, s, h]
+    var tapped = [false, false, false, false, false, false, false, false, false];
+    $('#music #top-row #b').on("click",function (){
+        if(tapped[0] === false){
+            madeon.play();
+            $('#body').css({'background-image':'url(assets/images/porteon.png)'});
+            tapped = true;
+        } else {
+            madeon.pause();
+            $('#body').css({'background-image':'url()'})
+        }
+    });
+}
